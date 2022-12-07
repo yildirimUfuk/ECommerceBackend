cf4e94e8021f782ccf95f180760b4730e5f0dbbd
yildirimUfuk <yildirimUfuk.esogu@gmail.com>
2022-12-07 20:39:44 +0300

    - Tracking mechanism implemented. If tracking is not necessary, it was turned off.

--- Core/ECommerceBackend.Application/Repositories/IReadRepository.cs
+++ Core/ECommerceBackend.Application/Repositories/IReadRepository.cs
@@ -10,10 +10,10 @@ namespace ECommerceBackend.Application.Repositories
 {
     public interface IReadRepository<T>:IRepository<T> where T : BaseEntity
     {
-        IQueryable<T> GetAll();
-        IQueryable<T> GetWhere(Expression<Func<T, bool>> method);
-        Task<T> GetSingleAsync(Expression<Func<T, bool>> method);
-        Task<T> GetByIdAsnc(string id);
+        IQueryable<T> GetAll(bool tracking=true);
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true);
+        Task<T> GetByIdAsnc(string id, bool tracking = true);
 
     }
 }
